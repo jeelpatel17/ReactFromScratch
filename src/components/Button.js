843aabc15d7c1377167e64d07ff35e26dae68c77
jeelpatel17 <jeelpateldev@gmail.com>
2022-02-19 18:03:27 +0530

    Initializing a new MERN project - Jeel's Notes

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button(props) {
-  return (
-    <button onClick={props.action} className="btn m-2 btn-outline-primary">
-      {props.text}
-    </button>
-  );
-}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App mode="light" />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
